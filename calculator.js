7333e8f562d16e0db869db3e64fbb7d540c7bddc
Richard Walsh <richwalsh2@googlemail.com>
2022-01-27 16:50:17 +0000

    calculator test

--- calculator.js
+++ calculator.js
@@ -1,7 +1,16 @@
-function calculator(inputOne, inputTwo) {
-  this.add = function () {
-    return inputOne + inputTwo;
-  };
-}
+const calculator = {
+  add: (input1, input2) => {
+    return input1 + input2;
+  },
+  subtract: (input1, input2) => {
+    return input1 - input2;
+  },
+  multiply: (input1, input2) => {
+    return input1 * input2;
+  },
+  divide: (input1, input2) => {
+    return input1 / input2;
+  },
+};
 
 module.exports = calculator;
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,17 @@
 const calculator = require('./calculator');
 
 test('Calculator Plus', () => {
-  expect(calculator(2, 2).add()).toBe(4);
+  expect(calculator.add(2, 2)).toBe(4);
+});
+
+test('Calculator subtract', () => {
+  expect(calculator.subtract(2, 2)).toBe(0);
+});
+
+test('Calculator multiply', () => {
+  expect(calculator.multiply(10, 2)).toBe(20);
+});
+
+test('Calculator divide', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
 });
